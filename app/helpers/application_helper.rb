9baa9defa52dbafd050ffa89acaa285e8e96de23
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-08-04 15:49:19 +0700

    add edit delete for category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def current_translations
+    @translations ||= I18n.backend.send(:translations)
+    @translations[I18n.locale].with_indifferent_access
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,13 @@ import Swal from 'sweetalert2/dist/sweetalert2.min.js'
 window.Swal = Swal;
 Rails.start()
 require("login/jquery-3.2.1.min")
+global.toastr = require("toastr")
 require("login/animsition.min")
 require("login/select2.min")
 require("admin/sweetalert2.min")
+//= require i18n
+//= require i18n.js
+//= require i18n/translations
 require("login/main")
 require("admin/index.min")
 Turbolinks.start()
